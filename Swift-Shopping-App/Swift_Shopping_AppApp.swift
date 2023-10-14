ffe838e6dbdc94d7cecf9f18c0935102e6adebf9
Ömer Türkmen <omerturkmen.sdet@gmail.com>
2023-10-14 17:54:48 +0300

    Initial Commit

---
+++ Swift-Shopping-App/Swift_Shopping_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Shopping_AppApp.swift
+//  Swift-Shopping-App
+//
+//  Created by ömer türkmen on 14.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Shopping_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
